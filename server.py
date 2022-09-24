19f0b99360c3b9eade538e8f69310da87b9ffafe
Victor Buendia <victorbuendia03@gmail.com>
2022-09-24 16:49:21 -0300

    :fire: | Ajustando buffer
    Co-authored-by: jvsdurso <jvsdurso@gmail.com>

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 import socket
 import select
 import sys
+import threading
 
 s = socket.socket()
 s.connect(('localhost', 1234))
 
+def limparBuffer():
+	print('> ', end = '')
+	sys.stdout.flush()
+
+threading.Thread(target=limparBuffer).start()
 while True:
 	readers, _, _ = select.select([sys.stdin, s], [], [])
 	for reader in readers:
 		if reader is s:
-			print('< ' + s.recv(1000).decode('utf8'))
+			print('< ' + s.recv(1000).decode('utf8') + '\n')
 		else:
+			threading.Thread(target=limparBuffer).start()
 			msg = sys.stdin.readline()
 			s.send(msg.encode('utf8'))
