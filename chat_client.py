67783b9777312932181d1bde2a2f323a2aefc2d9
Victor Buendia <victorbuendia03@gmail.com>
2022-09-25 16:03:29 -0300

    :fire: | Melhorando lógica do chat
    Co-authored-by: jvsdurso <jvsdurso@gmail.com>

--- chat_client.py
+++ chat_client.py
@@ -3,86 +3,116 @@ import select
 import errno
 import sys
 import threading
+import json
+import time
+import os
 
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
 PORT = 1234
-my_username = input("Username: ")
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def limparBuffer(my_username):
-    print(f'\n{my_username} > ', end = '')
+    print(f'\n{my_username} > ', end='')
     sys.stdout.flush()
 
-# Cria um socket com conexão TCP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Conecta em um ip e porta dados
-client_socket.connect((IP, PORT))
+def decode_message(receiver):
+    message = ''
+    has_message = True
 
-# Seta a conexão para não-blocante, para que o recv() não bloqueie, só retorne algum erro que trataremos adiante
-client_socket.setblocking(False)
+    while has_message:
+        try:
+            char = receiver.recv(1).decode('utf-8')
+            message += char
 
-# Prepara o username do participante e manda ele
-# Aqui precisamos codificar para bytes, depois contar o número de bytes e preparar o header com tamanho fixo, que também codificamos para bytes
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-client_socket.send(username_header + username)
+            if char == '}':
+                has_message = False
+        except KeyboardInterrupt as e:
+            print('\n\nO programa foi finalizado abruptamente.')
+            os._exit(1)
 
-threading.Thread(target=limparBuffer(my_username)).start()
+    return json.loads(message)
 
-while True:
-    readers,_,_ = select.select([sys.stdin, client_socket], [], [])
-    for reader in readers:
-        if reader is client_socket:
-            try:
-                # Agora fazemos um loop nas mensagens recebidas e as printamos
-                while True:
+def init():
+    my_room = input("Escolha a sala: ")
 
-                    # Recebe o header contendo o tamanho do username
-                    username_header = client_socket.recv(HEADER_LENGTH)
+    # Cria um socket com conexão TCP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-                    # Se não recebermos dados, o servidor irá encerrar a conexão, usando socket.close() ou socket.shutdown(socket.SHUT_RDWR)
-                    if not len(username_header):
-                        print('Conexão encerrada pelo servidor.')
+    # Conecta em um ip e porta dados
+    client_socket.connect((IP, PORT))
+
+    # Seta a conexão para não-blocante, para que o recv() não bloqueie, só retorne algum erro que trataremos adiante
+    client_socket.setblocking(False)
+
+    # Prepara o username do participante e manda ele
+    # Aqui precisamos codificar para bytes, depois contar o número de bytes e preparar o header com tamanho fixo, que também codificamos para bytes
+    # username = my_username.encode('utf-8')
+    # room = my_room.encode('utf-8')
+    data = {
+        'username': my_username,
+        'room': my_room,
+    }
+    encoded_data = json.dumps(data).encode('utf-8')
+    username_header = f"{len(encoded_data):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(username_header + encoded_data)
+
+    print('Para sair, digite o comando /SAIR.')
+    threading.Thread(target=limparBuffer(my_username)).start()
+
+    while True:
+        readers, _, _ = select.select([sys.stdin, client_socket], [], [])
+        for reader in readers:
+            if reader is client_socket:
+                try:
+                    message = decode_message(receiver=client_socket)
+                    sys.stdout.flush()
+                    print('\033[A \033[A')
+                    print(f'\n{message["username"]} > {message["message"]}')
+                    threading.Thread(target=limparBuffer(my_username)).start()
+
+                except IOError as e:
+                    # Se não vier dados, tentar novamente. Caso dê errado, sys.exit()
+                    if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                        print('Reading error: {}'.format(str(e)))
                         sys.exit()
+                    continue
 
-                    # Converte o header para inteiro
-                    username_length = int(username_header.decode('utf-8').strip())
-
-                    # Recebe e decodifica o username
-                    username = client_socket.recv(username_length).decode('utf-8')
-
-                    # Agora fazemos o mesmo para a mensagem (como recebemos o nome de usuário, recebemos a mensagem inteira, não há necessidade de verificar se ela tem algum comprimento)
-                    message_header = client_socket.recv(HEADER_LENGTH)
-                    message_length = int(message_header.decode('utf-8').strip())
-                    message = client_socket.recv(message_length).decode('utf-8')
-
-                    # Printa mensagem
-                    print(f'{username} > {message}')
-            except IOError as e:
-                # Se não vier dados, tentar novamente. Caso dê errado, sys.exit()
-                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                    print('Reading error: {}'.format(str(e)))
+                except Exception as e:
+                    # Se for uma exceção diferente, deu ruim, pode sair
+                    print('Reading error: '.format(str(e)))
                     sys.exit()
-                continue
+            else:
+                # Espera o usuário mandar uma mensagem
+                threading.Thread(target=limparBuffer(my_username)).start()
+                try:
+                    message = input()
+                except KeyboardInterrupt as e:
+                    print('\n\nO programa foi finalizado abruptamente.')
+                    os._exit(1)
 
-            except Exception as e:
-                # Se for uma exceção diferente, deu ruim, pode sair
-                print('Reading error: '.format(str(e)))
-                sys.exit()
-        else:
-            # Espera o usuário mandar uma mensagem
-            threading.Thread(target=limparBuffer(my_username)).start()
-            message = input()
-            # Se o usuário escrever sair ele sai do chat
-            if message == 'sair':
-                print("saindo...")
-                sys.exit()
-            # Se a mensagem não estiver vazia, mande ela
-            if message:
-                # Codifica a mensagem para bytes, prepara o header e converte para bytes, como o username acima, e depois mandar
-                message = message.encode('utf-8')
-                message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-                client_socket.send(message_header + message)
+                if message == "/SAIR":
+                    print('Saindo da sala...')
+                    client_socket.shutdown(socket.SHUT_RDWR)
+                    client_socket.close()
+                    time.sleep(1)
+                    clear()
+                    init()
 
+                # Se a mensagem não estiver vazia, mande ela
+                if message:
+                    # Codifica a mensagem para bytes, prepara o header e converte para bytes, como o username acima, e depois mandar
+                    message = message.encode('utf-8')
+                    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+                    client_socket.send(message_header + message)
+
+try:
+    my_username = input("Username: ")
+    init()
+except KeyboardInterrupt as e:
+    print('\n\nO programa foi finalizado abruptamente.')
+    os._exit(1)
--- chat_server.py
+++ chat_server.py
@@ -2,47 +2,23 @@ import socket
 import select
 import os
 import time
+import json
+import threading
 
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
 PORT = 1234
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
-
 comandos = {
     "/CRIAR": "Cria uma nova sala.",
+    "/LISTAR": "Listar salas criadas.",
+    "/ENCERRAR": "Encerrar uma sala.",
     "/SAIR": "Encerrar a aplicação.",
-    "/LISTAR": "Lista participantes"
+    "/LOGS": "Exibir logs"
 }
 
-def menu():
-    clear()
-    print('============ MENU ============')
-    for comando in comandos:
-        print(f'{comando} -----> {comandos[comando]}')
-    option = input('Digite um comando: ')
-    return option
-
-option = menu()
-while option not in comandos:
-    print('\nComando Inválido!')
-    time.sleep(1)
-    option = menu()
-
-if option == "/SAIR":
-    print('\nEncerrando aplicação...')
-    exit(0)
-if option == "/LISTAR":
-    print("listar participantes")
-if option == "/CRIAR":
-    print('\nInsira um número de 1234 a 9999 para criar uma sala')
-    numstr = input()
-    numint = int(numstr)
-    PORT = numint
-    print('\nCriando sala...')
+logs = []
 
 # Cria um socket com conexão TCP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -58,14 +34,132 @@ server_socket.listen()
 # Lista de sockets para o select()
 sockets_list = [server_socket]
 
-# Lista de clientes conectados (socket como chave, header do usuário e nome
+# Lista de clientes conectados (socket como chave, header do usuário e nome)
 clients = {}
 
-print(f'Esperando conexões em {IP}:{PORT}...')
+# Lista de salas abertas contendo limite de usuários e usuários conectados
+rooms = []
+
+
+def add_user_to_room(room_name, user):
+    room_selected = None
+    for room in rooms:
+        if room['name'] == room_name:
+            room_selected = room
+
+    if room_selected is not None:
+        room_limit = room_selected['limit']
+        if (len(room_selected['users']) + 1) <= room_limit:
+            room_selected['users'].append(user)
+            user['room'] = room_selected
+            clients[user['socket']] = user
+
+            notify_user_join_room(username_join=user['username'], room=room_selected)
+            log_message(f'{user["username"]} entrou na sala {room_selected["name"]}.')
+
+            return room_selected
+        else:
+            raise Exception('Limite da sala atingido.')
+    else:
+        raise Exception('Sala não encontrada.')
+
+
+def check_sockets_list():
+    for checking_socket in sockets_list:
+        if checking_socket.fileno() == -1:
+            sockets_list.remove(checking_socket)
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def create_room():
+    print('\nCriando sala...')
+
+    room_name = input('Digite o nome da sala: ')
+    room_limit = input('Digite o limite da sala: ')
+    room_id = (rooms[len(rooms) - 1]['id'] + 1) if (len(rooms) > 0) else 1
+    rooms.append({'id': room_id, 'name': room_name, 'limit': int(room_limit), 'users': []})
+
+    clear()
+    print(f'Sala {room_name} criada.')
+    log_message(f'Sala {room_name} criada. Atualmente, existem {len(rooms)} salas criadas.')
+
+
+def delete_room():
+    show_rooms()
+    room_to_be_removed = None
+
+    id_room_to_be_removed = input('Selecione o id da sala a ser removida:')
+    for room in rooms:
+        if room['id'] == int(id_room_to_be_removed):
+            room_to_be_removed = room
+            continue
+
+    if room_to_be_removed is not None:
+        remove_users_from_room(room=room_to_be_removed)
+        rooms.remove(room_to_be_removed)
+        print("Sala encerrada")
+        log_message(f'Sala {room_to_be_removed["name"]} foi encerrada.')
+        threading.Thread(target=manage_sockets).start()
+
+    else:
+        print('Sala não encontrada.')
+
+def init():
+    threading.Thread(target=manage_sockets).start()
+    log_message(f'Esperando conexões em {IP}:{PORT}...')
+    option = ''
+    first_time = 0
+    while True:
+        print(option)
+        match option.upper():
+            case '/CRIAR':
+                create_room()
+
+            case '/LISTAR':
+                show_rooms()
+                input('Pressione uma tecla para continuar...')
+                print('\n\n')
+
+            case '/LOGS':
+                show_logs()
+
+            case '/ENCERRAR':
+                delete_room()
+
+            case '/SAIR':
+                print('\nEncerrando aplicação...')
+                os._exit(1)
+
+            case _:
+                if first_time != 0:
+                    print('\nComando Inválido!')
+                    time.sleep(1)
+                first_time = 1
+        clear()
+        option = menu()
+
+
+def menu():
+    print('============ MENU ============')
+    for comando in comandos:
+        print(f'{comando} -----> {comandos[comando]}')
+    try:
+        option = input('Digite um comando: ')
+    except KeyboardInterrupt as e:
+        print('\n\nO programa foi finalizado abruptamente.')
+        os._exit(1)
+
+    return option
+
+
+def log_message(message):
+    logs.append(message)
+
 
 # Lidando com o recebimento de mensagem
 def receive_message(client_socket):
-
     try:
 
         # Recebe o "header" contendo o tamanho da mensagem
@@ -87,70 +181,137 @@ def receive_message(client_socket):
         # ou só perdeu a conexão
         return False
 
-while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-    # Iterar nos sockets notificados
-    for notified_socket in read_sockets:
+def manage_sockets():
+    while True:
+        check_sockets_list()
+        read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-        # Se o socket notificado é um socket servidor, aceitar a conexão
-        if notified_socket == server_socket:
+        # Iterar nos sockets notificados
+        for notified_socket in read_sockets:
 
-            # Aceita nova conexão
-            # Isso nos dá um novo soquete - soquete do cliente, conectado apenas a esse determinado cliente (exclusivo)
-            # O outro objeto retornado é ip/port set
-            client_socket, client_address = server_socket.accept()
+            # Se o socket notificado é um socket servidor, aceitar a conexão
+            if notified_socket == server_socket:
 
-            # Mandando o nome do cliente
-            user = receive_message(client_socket)
+                # Aceita nova conexão
+                # Isso nos dá um novo soquete - soquete do cliente, conectado apenas a esse determinado cliente (exclusivo)
+                # O outro objeto retornado é ip/port set
+                client_socket, client_address = server_socket.accept()
 
-            # Se for falso, o cliente caiu antes de mandar o nome
-            if user is False:
-                continue
+                # Mandando o nome do cliente
+                user = receive_message(client_socket)
+                # Se for falso, o cliente caiu antes de mandar o nome
+                if user is False:
+                    continue
 
-            # Adicionar socket aceito à lista do select
-            sockets_list.append(client_socket)
+                # Adicionar socket aceito à lista do select
+                sockets_list.append(client_socket)
 
-            # Salvar o username e o header
-            clients[client_socket] = user
+                # Salvar o username e o header
+                data = json.loads(user['data'].decode('utf-8'))
+                username = data['username']
+                room = data['room']
 
-            print('{} entrou na sala.'.format(user['data'].decode('utf-8')))
+                add_user_to_room(room_name=room, user={'socket': client_socket, 'username': username})
 
-        # Else, socket já existente está enviando uma mensagem
-        else:
-
-            # Recebe mensagem
-            message = receive_message(notified_socket)
-
-            # Se for falso, cliente disconectou, pode limpar
-            if message is False:
-                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
-
-                # Remove da lista do socket
+            # Else, socket já existente está enviando uma mensagem
+            else:
+                send_message(socket=notified_socket)
+        for notified_socket in exception_sockets:
+            try:
                 sockets_list.remove(notified_socket)
-
-                # Remove da lista de usuários
                 del clients[notified_socket]
+            except Exception as e:
+                log_message(f'Erro ao remover socket que deu errado: {e}')
 
-                continue
 
-            # Pegar o usuário do socket notificado, para saber quem enviou a mensagem
-            user = clients[notified_socket]
+def notify_user_left_room(username_left, room):
+    for user in room['users']:
+        data = {'username': username_left, 'message': '*** SAIU DA SALA ***'}
+        encoded_data = json.dumps(data).encode('utf-8')
+        try:
+            user['socket'].send(encoded_data)
+        except Exception as e:
+            log_message(f'Não foi possível enviar mensagem: {e}')
 
-            print(f'{user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
 
-            # Iterar sobre os clientes conectados e a mensagem broadcast
-            for client_socket in clients:
+def notify_user_join_room(username_join, room):
+    for user in room['users']:
+        data = {'username': username_join, 'message': '*** ENTROU NA SALA ***'}
+        encoded_data = json.dumps(data).encode('utf-8')
+        user['socket'].send(encoded_data)
 
-                # Não enviar para quem enviou
-                if client_socket != notified_socket:
 
-                    # Enviar usuário e mensagem com seus headers
-                    # Reutilizando header da mensagem enviada pelo transmissor e salvando username enviado pelo usuário na conexão
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+def remove_users_from_room(room):
+    for user in room['users']:
+        data = {'username': 'Sistema', 'message': '*** ESTA SALA FOI ENCERRADA ***'}
+        encoded_data = json.dumps(data).encode('utf-8')
+        user['socket'].send(encoded_data)
 
-    for notified_socket in exception_sockets:
+        user['socket'].shutdown(socket.SHUT_RDWR)
+        user['socket'].close()
 
-        sockets_list.remove(notified_socket)
 
-        del clients[notified_socket]
+def remove_socket(socket_closed):
+    user_left = clients[socket_closed]
+    room_user_left = user_left['room']
+    room_user_left['users'].remove(user_left)
+
+    notify_user_left_room(username_left=user_left['username'], room=room_user_left)
+
+    del clients[socket_closed]
+    sockets_list.remove(socket_closed)
+    log_message(f'Conexão encerrada de: {user_left["username"]}')
+
+
+def send_message(socket):
+    # Recebe mensagem
+    message = receive_message(socket)
+
+    # Se for falso, cliente desconectou, pode limpar
+    if message is False:
+        remove_socket(socket)
+        return
+
+    # Pegar o usuário do socket notificado, para saber quem enviou a mensagem
+    sender_user = clients[socket]
+    username = sender_user['username']
+
+    log_message(f'{username} [{sender_user["room"]["name"]}]: {message["data"].decode("utf-8")}')
+
+    # Iterar sobre os clientes conectados e a mensagem broadcast
+    for user in sender_user['room']['users']:
+
+        # Não enviar para quem enviou
+        if user['socket'] != socket:
+            data = {'username': username, 'message': message['data'].decode('utf-8')}
+            encoded_data = json.dumps(data).encode('utf-8')
+
+            # Enviar usuário e mensagem com seus headers
+            # Reutilizando header da mensagem enviada pelo transmissor e salvando username enviado pelo usuário na conexão
+            user['socket'].send(encoded_data)
+
+
+def show_logs():
+    clear()
+    print('****** LOGS ******')
+
+    for log in logs:
+        print(f'- {log}')
+
+    input('Pressione uma tecla para continuar...')
+    print('\n\n')
+
+
+def show_rooms():
+    clear()
+    print('****** SALAS ******')
+    for room in rooms:
+        print(f'{room["id"]}: {room["name"]} - {len(room["users"])} usuários conectados.')
+
+# Iniciar o programa
+try:
+    init()
+except KeyboardInterrupt as e:
+    print('\n\nO programa foi finalizado abruptamente.')
+    os._exit(1)
